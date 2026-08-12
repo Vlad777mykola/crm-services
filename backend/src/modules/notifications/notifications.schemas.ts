224ed334652cb935677ccab8d6ae199ce4847b27
Vlad777mykola <vlad777mykola@gmail.com>
2026-08-12 19:37:22 +0300

    0.17: remove legacy folder

--- backend/src/modules/notifications/notifications.schemas.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const notificationIdParamsSchema = z.object({
-  notificationId: z.string().uuid(),
-});
-
-export type NotificationIdParams = z.infer<typeof notificationIdParamsSchema>;
